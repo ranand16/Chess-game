30627d6c25e0bd7b80602d9654d08c2278b9275e
Rishabh Anand <ranand16@gmail.com>
2020-06-12 20:41:42 +0530

    Added highlight to chess board after click.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,9 @@ class App extends React.Component {
     super(props);
     console.log(props);
     this.state = {
-      joinScreen: "join", // can be false, "create" as well
-      gameScreen: false,
+      // joinScreen: "join", // can be false, "create" as well
+      joinScreen: false, // can be false, "create" as well
+      gameScreen: true,
       userNameInput: "", // used in onchange handler and the final value
       roomNameInput: "", // used in onchange handler and the final value
 
@@ -24,23 +25,22 @@ class App extends React.Component {
       onGoingMoveTwo: false, // second click to select a cell to end the move
 
       playerSide: true, // true for dark and false for white
-      chessGameData: 
-      [
+      chessGameData: [
         ["rook_dark", "knight_dark", "bishop_dark", "queen_dark", "king_dark", "bishop_dark", "knight_dark", "rook_dark"],
         ["pawn_dark", "pawn_dark", "pawn_dark", "pawn_dark", "pawn_dark", "pawn_dark", "pawn_dark", "pawn_dark"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
+        ["na", "na", "na", "na", "na", "na", "na", "na"],
+        ["na", "na", "na", "na", "na", "na", "na", "na"],
+        ["na", "na", "na", "na", "na", "na", "na", "na"],
+        ["na", "na", "na", "na", "na", "na", "na", "na"],
         ["pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white", "pawn_white"],        
         ["rook_white", "knight_white", "bishop_white", "queen_white", "king_white", "bishop_white", "knight_white", "rook_white"],
-      ]
-
+      ],
+      probableDestinations: []
     }
   }
 
   componentDidMount(){
-    socket = io('localhost:5000')
+    // socket = io('localhost:5000')
   }
 
   userNameChange = (e) => {
@@ -55,59 +55,68 @@ class App extends React.Component {
     }
   }
 
-  clickCell = (e) => {
+  clickCell = (i ,j, e) => {
+    console.log(i, j)
     let classArray = darkClassArray;
     let clickedPlayer = null;
-    const { onGoingMoveOne, onGoingMoveTwo, playerSide } = this.state;  
+    const { onGoingMoveOne, onGoingMoveTwo, playerSide, chessGameData } = this.state;  
     if(!playerSide) classArray = whiteClassArray;
-    let classnames = e.target.className
-    let selectedClassArray = classnames.split(" ");
-    let commonClassArray = selectedClassArray.filter(function(n) {
-      return classArray.indexOf(n) !== -1;
-    })
-    if(commonClassArray.length<1) return // you have clicked an empty cell 
-    clickedPlayer = commonClassArray[0];
+    clickedPlayer = chessGameData[i][j];
+    console.log(onGoingMoveOne, onGoingMoveTwo)
     if(onGoingMoveOne && !onGoingMoveTwo){
       // start the second step
     } else if(onGoingMoveOne && onGoingMoveTwo){
       // complete the move
       this.setState({ onGoingMoveOne: false, onGoingMoveTwo: false })
     } else {
+      // check if the clicked cell is not empty
+      if(clickedPlayer && clickedPlayer==="na") return // you have clicked an empty cell 
+      console.log(clickedPlayer)
       // start the first step of move
-      // this.setState({ onGoingMoveOne: true, onGoingMoveTwo: false })
       // check type of selected player & highlight the possible destinations
-      console.log(classArray)
-      switch(clickedPlayer){
-        case classArray[0]: // rook
-          console.log(clickedPlayer)
-
-        break;
-        case classArray[1]: // knight
-          console.log(clickedPlayer)
-        break;
-        case classArray[2]: // bishop
-          console.log(clickedPlayer)
-        break;
-        case classArray[3]: // queen 
-          console.log(clickedPlayer)
-        break;
-        case classArray[4]: // king
-          console.log(clickedPlayer)
-        break;
-        case classArray[5]: // pawn
-          console.log(clickedPlayer)
-
-        break;
-        default:
-        break;
-      }
-
+      let probableDestinations = this.calculateDestinations(parseInt(i), parseInt(j), clickedPlayer, playerSide, classArray, chessGameData)
+      // this.setState({ onGoingMoveOne: true, onGoingMoveTwo: false, probableDestinations })
+      this.setState({ probableDestinations })
     }
+  }
+
+  calculateDestinations = (i, j, playerType, playerSide, classArray, chessGameData) => {
+    console.log(i, j, playerType, classArray)
+    let hightlightArray = [];
+    switch(playerType){
+      case classArray[0]: // rook
+        // console.log(playerType)
+      break;
+      case classArray[1]: // knight
+        console.log(playerType)
+      break;
+      case classArray[2]: // bishop
+        console.log(playerType)
+      break;
+      case classArray[3]: // queen 
+        console.log(playerType)
+      break;
+      case classArray[4]: // king
+        console.log(playerType)
+      break;
+      case classArray[5]: // pawn
+        if(playerSide){ // dark
+          hightlightArray.push({ x:i+1, y:j, enemyCell: chessGameData[i+1][j]==="na"?false:true })
+          if(i===1) hightlightArray.push({ x:i+2, y:j, enemyCell: chessGameData[i+2][j]==="na"?false:true });
+        } else { // white
+          hightlightArray.push({ x:i-1, y:j, enemyCell: chessGameData[i-1][j]==="na"?false:true });
+          if(i===6) hightlightArray.push({ x:i-2, y:j, enemyCell: chessGameData[i-2][j]==="na"?false:true });
+        }
+      break;
+      default:
+      break;
+    }
+    return hightlightArray
   }
 
   enterGame = (type) => { // create or join
     console.log(type)
-    const { userNameInput, roomNameInput } = this.state
+    const { userNameInput, roomNameInput, chessGameData, probableDestinations } = this.state
     if(userNameInput && roomNameInput){
       socket.emit("joinroom", { userName: userNameInput, roomName: roomNameInput }, type, (response)=>{
         console.log(response);
@@ -140,7 +149,8 @@ class App extends React.Component {
   }
 
   render (){
-    const { joinScreen, gameScreen, chessGameData, currentRoomId, currentUserId } = this.state
+    const { joinScreen, gameScreen, chessGameData, probableDestinations, currentRoomId, currentUserId } = this.state
+    // console.log(probableDestinations)
     return (
       <div className="App">
         {joinScreen && <div id="joinScreen">
@@ -158,86 +168,27 @@ class App extends React.Component {
         {gameScreen && <div id="gameScreen">
           <div id="gameLeftPane">This is left pane</div>
           <div id="gamePane">
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[0][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[0][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[0][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[0][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[0][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[0][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[0][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[0][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell blackBackground ${chessGameData[1][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[1][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[1][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[1][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[1][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[1][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[1][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[1][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[2][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[2][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[2][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[2][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[2][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[2][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[2][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[2][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell blackBackground ${chessGameData[3][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[3][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[3][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[3][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[3][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[3][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[3][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[3][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[4][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[4][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[4][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[4][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[4][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[4][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[4][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[4][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell blackBackground ${chessGameData[5][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[5][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[5][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[5][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[5][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[5][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[5][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[5][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[6][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[6][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[6][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[6][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[6][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[6][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[6][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[6][7]}`} onClick={this.clickCell}></div>
-            </div>
-            <div className={"gamePaneRow"}>
-              <div className={`gamePaneCell blackBackground ${chessGameData[7][0]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[7][1]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[7][2]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[7][3]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[7][4]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[7][5]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell blackBackground ${chessGameData[7][6]}`} onClick={this.clickCell}></div>
-              <div className={`gamePaneCell whiteBackground ${chessGameData[7][7]}`} onClick={this.clickCell}></div>
-            </div>
+            {
+              chessGameData.map((data, i)=>{
+                return (
+                  <div key={`${i}`} className={"gamePaneRow"}>
+                    {data.map((eachData, j)=>{
+                      let isProbableDestination = false
+                      let isDanger = false
+                      for(let d=0;d<probableDestinations.length;d++){
+                        if(probableDestinations[d]["enemyCell"]){
+                          isDanger = probableDestinations[d]["enemyCell"]?probableDestinations[d]["enemyCell"]:false
+                        }
+                        isProbableDestination = probableDestinations[d]["x"] === i && probableDestinations[d]["y"] === j
+                        console.log(isProbableDestination, probableDestinations[d]["x"], i, probableDestinations[d]["y"], j)
+                      }
+                      let classNames = `gamePaneCell ${((i%2===0 && j%2===0) || (i%2!==0 && j%2!==0))?("whiteBackground"):("blackBackground")} ${isProbableDestination?"highlight":null} ${isDanger?"highlight_danger":""} ${chessGameData[i][j]}`;
+                      return ( <div key={`${i}+${j}`} className={classNames} onClick={this.clickCell.bind(this,i,j)}></div> )
+                    })}
+                  </div>
+                )
+              })
+            }
           </div>
           <div id="gameRightPane">This is right pane</div>
         </div>}
@@ -247,28 +198,3 @@ class App extends React.Component {
 }
 
 export default App;
- 
-  // {
-  //   "status": true,
-  //   "data": {
-  //     "room": {
-  //       "id": "ec7c7f34-6020-4426-acc1-8ce27676e5c7",
-  //       "name": "Sector 51",
-  //       "spectators": [
-  //         {
-  //           "id": "5d393a3e-169c-4041-a29f-d5c66d8b10c3",
-  //           "name": "Rishabh"
-  //         },
-  //         {
-  //           "id": "44682458-b3e0-4c09-a548-3c6c7e3fc564",
-  //           "name": "Rishabh"
-  //         }
-  //       ],
-  //       "players": []
-  //     },
-  //     "user": {
-  //       "id": "44682458-b3e0-4c09-a548-3c6c7e3fc564",
-  //       "name": "Rishabh"
-  //     }
-  //   }
-  // }
