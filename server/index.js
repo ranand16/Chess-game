01840173b06ecd60c32b4ba01864f23eee988bc0
Rishabh Anand <ranand16@gmail.com>
2020-06-16 08:34:19 +0530

    Backend update

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const { uuid } = require('uuidv4');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
-const socket = socketio(server);
+const socketserve = socketio(server);
 
 let rooms = [];
 let users = [];
-socket.on('connection', (socket)=>{
+socketserve.on('connection', (socket)=>{
     socket.on('joinroom', ({userName, roomName}, type="join", callback)=>{
         const response = { status: true, data: {} }
         let room; let user;
@@ -24,43 +24,59 @@ socket.on('connection', (socket)=>{
             if(room && type==="join" ) {// There is a room which is already created. So this is JOIN
                 room["spectators"].push(user)
             } else if(!room && type==="create") { // A new room is going to be created
-                room = { id: uuid(), name: roomName, spectators: [user], players: [] }
+                room = { id: uuid(), name: roomName, spectators: [user], players: ["", ""] }
                 rooms.push(room)
             }
             // add this user's details
             users.push(user);
+            socket.join(room.id);
+            socketserve.sockets.in(room.id).emit('updateState', { room })
             response["data"] = {room, user}
             callback(response)
-            console.log(`Current rooms`);
-            console.log(rooms);
-            console.log(`Current users`); 
-            console.log(users);
         } catch(err) {
             response["status"] = false
             response["data"] = err["data"] || "There was some kind of error."
             callback(response)
         }
     });
-    socket.on('participate', (userId, roomId, callback)=>{
-        let room; let user; let response = {status: true, data: {}};
+    socket.on('participate', (userId, roomId, playerSide, callback)=>{
+        let room; let user; let response = {status: true, data: {}}; let roomIndex; let userIndex; 
+        console.log(userId, roomId, playerSide, callback)
         try{ 
             if(!userId && !roomId) throw { status: false, data: "User id and Room id is mandatory." }
-            room = romms.find(room => room.id === roomId)
-            user = users.find(user => user.id === userId)
+            room = rooms.find((currRoom, i) => {
+                roomIndex = i;
+                return (currRoom.id == roomId);
+            })
+            user = users.find((user, j) => {
+                userIndex = j;
+                return (user.id == userId);
+            })
+            // room["spectators"].find()
             if(!room && !user) throw { status: false, data: "User and Room were not found in the database." }
-            if(room["players"].length < 2) room["players"].push(user)
-            response["data"] = `Player ${user.name} is a participant now.`
+            console.log(playerSide, room["players"], room["players"][0], room["players"][1])
+            if(room["players"].length <= 2) { 
+                if(playerSide === "dark" && room["players"][0]==="") room["players"][0] = user;
+                else if(playerSide === "white" && room["players"][1]==="") room["players"][1] = user;
+            }
+            console.log(room)
+            socketserve.sockets.in(room.id).emit('updateState', { room })
+            response["data"] = { players: room["players"], room }
+            callback(response)
         } catch(err) {
             response["status"] = false
+            console.log(err)
             response["data"] = err["data"] || "There was some kind of error."
+            callback(response)
         }
-        callback(response)
-
-    })
+    });
     socket.on('disconnect', (name, room)=>{
         console.log(`${name} wants to disconnect from the room name ${room}`);
-        // callback(name, room);
     });
+    // socket.on('updateState', (roomId, callback)=>{
+    //     room = rooms.find(currRoom => currRoom.id == roomId)
+    //     callback(room)
+    // });
 });
 server.listen(PORT, ()=>{
     console.log(`server running at ${PORT}`)
